116cc7dcfae5c1fb59cc3658fa56c8a0ea7fa889
zeta <zezeon1@gmail.com>
2020-04-30 08:52:39 +0900

    =

--- biz_src/opencv.cc
+++ biz_src/opencv.cc
@@ -288,6 +288,7 @@ void Biz::index()
 	}
 
 	if(id_ != "") {//if already logged
+		swap("hidden", "visible");//show logout button
 		regex e{R"(<div id=login[\s\S]+?</form>\s+</div>)"};
 		content_ = regex_replace(content_, e, id_ + "님 로그인되었습니다.",
 				regex_constants::format_first_only);
