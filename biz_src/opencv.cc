1514d92265937d57fdd9349d1a35f940ba586ea9
zeta <zezeon1@gmail.com>
2020-04-30 01:09:02 +0900

    =

--- biz_src/opencv.cc
+++ biz_src/opencv.cc
@@ -288,7 +288,7 @@ void Biz::index()
 
 	if(id_ != "") {//if already logged
 		swap("hidden", "visible");//show logout button
-		regex e{R"(<form[\s\S]+?</form>)"};
+		regex e{R"(<div id=login[\s\S]+?</form>\s+</div>)"};
 		content_ = regex_replace(content_, e, id_ + "님 로그인되었습니다.",
 				regex_constants::format_first_only);
 		sq.select("bcard", "where user = '" + id_ + "' order by name");
