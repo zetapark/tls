f49e572620b404b76db701e59a925e7bf1f0bffc
zeta <zezeon1@gmail.com>
2020-05-16 13:18:45 +0900

    =

--- ez/src/ez.cpp
+++ ez/src/ez.cpp
@@ -5,6 +5,8 @@
 #include<tcpip/bootstrap.h>
 using namespace std;
 
+string car;
+
 class MySite : public BootStrapSite
 {
 protected:
@@ -16,16 +18,7 @@ protected:
 		} else if(requested_document_ == "korean") content_ = language = "korean";
 		else if(requested_document_ == "english") content_ = language = "english";
 		else if(requested_document_ == "tutorial.js") if(language == "korean") swap("kor", "eng");
-		else if(requested_document_ == "carousel") {
-			ifstream f("carousel.txt");
-			int n; string s; vector<string> v[3];
-			f >> n; getline(f, s);
-			for(int i=0; i<n; i++) for(int j=0; j<3; j++) {
-				getline(f, s);
-				v[j].push_back(s);
-			}
-			content_ = carousel(v[0], v[1], v[2]);
-		}
+		else if(requested_document_ == "index.html") swap("@CAROUSEL", car);
 		cout << requested_document_ << endl;
 	}
 	string language = "korean";
@@ -38,6 +31,15 @@ int main(int ac, char** av)
 		{"port", "listening port", 2001}
 	};
 	if(!co.args(ac, av)) return 0;
+
+	ifstream f("carousel.txt");
+	int n; string s; vector<string> v[3];
+	f >> n; getline(f, s);
+	for(int i=0; i<n; i++) for(int j=0; j<3; j++) {
+		getline(f, s);
+		v[j].push_back(s);
+	}
+	car = carousel(v[0], v[1], v[2]);
 
 	MySite f;
 	f.init(co.get<const char*>("dir"));
