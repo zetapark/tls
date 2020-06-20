adad5c9f345d8023740970755f20348d17e03998
zeta <zezeon1@gmail.com>
2020-06-20 21:11:57 +0900

    =

--- adnet/src/ad.cpp
+++ adnet/src/ad.cpp
@@ -17,6 +17,7 @@ int main(int ac, char** av)
 	Ad site;
 	site.init(co.get<const char*>("dir"));
 	site.add_header("request_ad.php", "Access-Control-Allow-Origin: *");
+	site.add_header("adnet.js", "Access-Control-Allow-Origin: *");
 	Server sv{co.get<int>("port")};
 	return sv.keep_start(site);
 }
