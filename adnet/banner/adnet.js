a334b9e0328e1926567a6392e9206d7d63b9f6d8
zeta <zezeon1@gmail.com>
2020-06-20 14:01:57 +0900

    =

---
+++ adnet/banner/adnet.js
@@ -0,0 +1,30 @@
+function request_ad() {
+	$.post('https://ad.zeta2374.com/request_ad.php', { id : $('#adnet').attr('myid') },
+		function(data) {
+			var split = data.split('\n');
+			$('#adnet').attr('src', split[0] + '-' + $('#adnet').attr('adtype') + '.jpg');//img
+			$('#adnet').off('click');
+			$('#adnet').click(function() {
+				$.post('https://ad.zeta2374.com/clicked', {id : $('#adnet').attr('myid'),
+					click : split[0], token : split[2]});//id
+				window.location.href = split[1];//link
+			});
+		}
+	);
+}
+
+$(function() {
+	$('#adnet').error(function() {
+		$('#adnet').attr('src', 'https://tomcat.zeta2374.com/banner/adnet-' +
+			$('#adnet').attr('adtype') + '.jpg');
+		$('#adnet').off('click');
+		$('#adnet').click(function() {
+			$.post('https://ad.zeta2374.com/clicked.php', {id : $('#adnet').attr('myid'),
+				click : 'adnet'});
+			window.location.href = 'https://adnet.zeta2374.com';
+		});
+	});
+
+	request_ad();
+	setInterval(request_ad, 10000);
+});
