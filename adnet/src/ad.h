84679edca5be16852acf263d59237b5f6842a84b
zeta <zezeon1@gmail.com>
2020-06-20 16:59:21 +0900

    =

--- adnet/src/ad.h
+++ adnet/src/ad.h
@@ -19,7 +19,7 @@ private:
 	bool check_token(std::string token);
 	std::string new_token();
 
-	std::chrono::time_point<std::chrono::system_clock> last_save_;
+	std::chrono::time_point<std::chrono::system_clock> last_save_ = std::chrono::system_clock::now();
 	std::set<std::string> token_, prev_token_;
 	std::map<std::string, int> view_increase_, click_increase_, view_induce_, click_induce_;
 	std::uniform_int_distribution<> di_{0, 10}; std::random_device rd_;
