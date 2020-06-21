9728f966835d0cc1953b485f7378bcdc6e7320b1
zeta <zezeon1@gmail.com>
2020-06-21 14:47:27 +0900

    =

--- suwon/src/suwon.cpp
+++ suwon/src/suwon.cpp
@@ -9,16 +9,17 @@ class MyWeb : public WebSiteIP
 {
 protected:
 	void process(sockaddr_in) {
-		sq.connect("192.168.0.3", "kk", "kk", "kyungki");
-		sq.select("가맹점", "where 상호 like '%" + nameNvalue_["name"] + "%' and 주소 like '%" 
-				+ nameNvalue_["adress"] + "%' and 업종 like '%" + nameNvalue_["kind"] + "%' limit 100");
-		string s;
-		for(auto a : sq) {
-			s += "<tr><td>" + a["상호"].asString() + "</td><td>" + a["주소"].asString()
-				+ "</td><td>" + a["업종"].asString() + "</td><td>" + a["전화번호"].asString()
-				+ "</td></tr>";
+		if(string s; requested_document_ == "index.html") {
+			sq.connect("192.168.0.3", "kk", "kk", "kyungki");
+			sq.select("가맹점", "where 상호 like '%" + nameNvalue_["name"] + "%' and 주소 like '%" +
+					nameNvalue_["adress"] + "%' and 업종 like '%" + nameNvalue_["kind"] + "%' limit 100");
+			for(auto a : sq) {
+				s += "<tr><td>" + a["상호"].asString() + "</td><td>" + a["주소"].asString()
+					+ "</td><td>" + a["업종"].asString() + "</td><td>" + a["전화번호"].asString()
+					+ "</td></tr>";
+			}
+			append("<tbody>", s);
 		}
-		append("<tbody>", s);
 	}
 	SqlQuery sq;
 };
