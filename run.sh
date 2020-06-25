7275a46471d5d6fea2d4b7955b4d5f9088068174
zeta <zezeon1@gmail.com>
2020-06-25 19:39:54 +0900

    =

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 while : ; do
+	cd middle
 	./https.src.x
+	cd ..
 	sleep 10
 done &
 while : ; do
+	cd dndd
 	./dndd.dndd.x
+	cd ..
 	sleep 10
 done &
 while : ; do
+	cd middle
 	python3 pybind/gensim-server.py
+	cd ..
 	sleep 10
 done &
