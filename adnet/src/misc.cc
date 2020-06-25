c892276060c434456822eb05ccc1ad332604d45d
zeta <zezeon1@gmail.com>
2020-06-25 17:57:16 +0900

    =

--- adnet/src/misc.cc
+++ adnet/src/misc.cc
@@ -1,8 +1,10 @@
+#include<iostream>
 #include"misc.h"
 using namespace std;
 
 void Misc::process(sockaddr_in&&) {
 	if(requested_document_ == "leave_message") {
+		cout << nameNvalue_["email"] << endl;
 		if(nameNvalue_["dest"] == "") nameNvalue_["dest"] = "zeta@zeta2374.com";
 		string cmd = "mailx " + nameNvalue_["dest"] + " -r " + nameNvalue_["email"] + " -s '" +
 			nameNvalue_["title"] + "' <<HERE_CONTENT\n" + nameNvalue_["content"] + "\nHERE_CONTENT";
