5524851c75a58d690b122c127693ec3e91b2cf37
zeta <zezeon1@gmail.com>
2020-09-13 12:56:37 +0900

    =

--- run.sh
+++ run.sh
@@ -1,20 +1,44 @@
-#!/bin/bash
-
 while : ; do
-	cd lib 
-	./middle.tls.x -port 4433 
+	cd lib
+	./middle.tls.x
+	cd ..
+	sleep 10
+done &
+while : ; do
+	cd adnet
+	./misc.src.x -dir misc -port 2003
+	cd ..
+	sleep 10
+done &
+while : ; do
+	cd adnet
+	./ad.src.x -dir ad -port 2002
+	cd ..
+	sleep 10
+done &
+while : ; do
+	cd adnet
+	./adnet.src.x -dir www -port 2004 -data localhost
 	cd ..
 	sleep 10
 done &
 while : ; do
 	cd biz
-	./biz.src.x -port 2001
+	./biz.src.x -dir www -port 2001 -data localhost
 	cd ..
 	sleep 10
 done &
+
 while : ; do
 	cd middle
-	python3 pybind/gensim-server.py
+	./gensim-server.py
+	cd ..
+	sleep 10
+done &
+
+while : ; do
+	cd adnet
+	./adnet2.src.x -dir Bikin -port 2005 -data localhost
 	cd ..
 	sleep 10
 done &
