4f6c9f794df25ee11e1b72eddd39af6d6f6e82b5
zeta <zezeon1@gmail.com>
2020-09-22 22:24:30 +0900

    1

--- adnet/ad/adnet.js
+++ adnet/ad/adnet.js
@@ -1,17 +1,17 @@
 function request_ad() {
-	if(document.hasFocus() && $('#adnet').is(':visible')) {
+	if(document.hasFocus() && $('#adnet2').is(':visible')) {
 		$.post(
 			'https://ad.zeta2374.com/request_ad.php',
-			{ id : $('#adnet').attr('myid'), category : $('#adnet').attr('category'), 
+			{ id : $('#adnet2').attr('myid'), category : $('#adnet2').attr('category'), 
 				url : window.location.href },
 			function(data) {
 				var split = data.split('\n');
-				$('#adnet').attr('src', 'https://tomcat.zeta2374.com/banner/' + split[0] 
-					+ '-' + $('#adnet').attr('adtype') + '.jpg');//img
-				$('#adnet').off('click');
-				$('#adnet').click(function() {
+				$('#adnet2').attr('src', 'https://tomcat.zeta2374.com/banner/' + split[0] 
+					+ '-' + $('#adnet2').attr('adtype') + '.jpg');//img
+				$('#adnet2').off('click');
+				$('#adnet2').click(function() {
 					$.post('https://ad.zeta2374.com/click_ad.php',
-						{id : $('#adnet').attr('myid'), click : split[0], token : split[2]});//id
+						{id : $('#adnet2').attr('myid'), click : split[0], token : split[2]});//id
 					window.open(split[1], '_blank');//link
 				});
 			}
@@ -20,29 +20,29 @@ function request_ad() {
 }
 
 $(function() {
-	var tmp = $('#adnet').attr('src');
-	$('#adnet').on('error', function() {
-		$('#adnet').on('error', function() {
-			$('#adnet').attr('src', tmp);
-			$('#adnet').off('click');
+	var tmp = $('#adnet2').attr('src');
+	$('#adnet2').on('error', function() {
+		$('#adnet2').on('error', function() {
+			$('#adnet2').attr('src', tmp);
+			$('#adnet2').off('click');
 		});
-		$('#adnet').attr('src', 'https://tomcat.zeta2374.com/banner/adnet-' +
-			$('#adnet').attr('adtype') + '.jpg');
-		$('#adnet').off('click');
-		$('#adnet').click(function() {
-			$.post('https://ad.zeta2374.com/click_ad.php', {id : $('#adnet').attr('myid'),
-				click : 'adnet'});
-			window.open('https://adnet.zeta2374.com', '_blank');
+		$('#adnet2').attr('src', 'https://tomcat.zeta2374.com/banner/adnet2-' +
+			$('#adnet2').attr('adtype') + '.jpg');
+		$('#adnet2').off('click');
+		$('#adnet2').click(function() {
+			$.post('https://ad.zeta2374.com/click_ad.php', {id : $('#adnet2').attr('myid'),
+				click : 'adnet2'});
+			window.open('https://adnet2.zeta2374.com', '_blank');
 		});
 	});
 
-	$('#adnet').bind('contextmenu', function() {
-		window.open('https://adnet.zeta2374.com', '_blank');
+	$('#adnet2').bind('contextmenu', function() {
+		window.open('https://adnet2.zeta2374.com', '_blank');
 	});
 
 	request_ad();
-	var interval = parseInt($('#adnet').attr('interval'), 10) || 0;
+	var interval = parseInt($('#adnet2').attr('interval'), 10) || 0;
 	if(interval < 10) interval = 10;
 	setInterval(request_ad, interval * 1000);
-	$('#adnet').css('cursor', 'pointer');
+	$('#adnet2').css('cursor', 'pointer');
 });
